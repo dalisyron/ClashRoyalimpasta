191a1729e6fb39b4c5d4444e3ce0c443c1abbabc
Mohamad sadegh khavari <mohamadsadeghkhavari@Mohamads-MacBook-Pro.local>
2018-01-21 17:14:46 +0330

    new data

--- Data.py
+++ Data.py
@@ -1,6 +1,19 @@
+import pygame
 
-Heros_Dic={"Big_Hero" : {"DAMAGE_RATE":90 ,"SPEED_RATE":10,"HEALTH":100,"BULLET":None },
-           "knight" :{ "DAMAGE_RATE":90 ,"SPEED_RATE":10,"HEALTH":100,"BULLET":"Sword" }
+Heros_Dic={"Big_Hero" :
+               {"DAMAGE_RATE":90 ,
+                "SPEED_RATE":10,
+                "HEALTH":100,
+                "BULLET":None ,
+                'IMAGE':pygame.image.load('Images/Big_Hero.gif')},
+           "knight" :{
+               "DAMAGE_RATE":90 ,
+               "SPEED_RATE":10,
+               "HEALTH":100,
+               "BULLET":"Sword",
+                'IMAGE':pygame.image.load('Images/Knight.gif')}
            }
 
 Bullets_Dic={"Sword" : {"DAMAGE" : 30 , "SPEED" : 40 , "RANGE" : 3 }}
+
+image = {'Big_Hero':pygame.image.load('Images/Big_Hero.gif'), }
