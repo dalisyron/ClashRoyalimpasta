d67ad060ccc65cdb566da10a5714565775506c19
Mohamad sadegh khavari <mohamadsadeghkhavari@Mohamads-MacBook-Pro.local>
2018-01-23 18:29:26 +0330

    Prossing Bullets and Heros added to the Modules

--- Bullet.py
+++ Bullet.py
@@ -2,16 +2,37 @@ import Data
 
 class Bullet:
 
-    def __init__(self,x1,y1,x2,y2,name):
+    def __init__(self,x1,y1,x2,y2,name,side_):
         self.Name=name
+        self.side=side_
         bullet_atributes=Data.Bullets_Dic[name]
-        self.Position_x=x1
-        self.Position_y = y1
-        self.Dest_x = x2
-        self.Dest_y = y2
-        self.Range = bullet_atributes["RANGE"]
-        self.Damage = bullet_atributes["DAMAGE"]
-        self.Speed = bullet_atributes["SPEED"]
+        self.position_x=x1
+        self.position_y = y1
+        self.dest_x = x2
+        self.dest_y = y2
+        self.range = bullet_atributes["RANGE"]
+        self.damage = bullet_atributes["DAMAGE"]
+        self.speed = bullet_atributes["SPEED"]
+
+
+
+
+
+
+
+
+
+def bulletsProcess(currentBullets):
+    for bullet in currentBullets:
+        dx=bullet.dest_x - bullet.position_x
+        dy=bullet.dest_y - bullet.position_y
+        d=(dx**2 + dy**2)**0.5
+        vx=bullet.speed*(dx/d)
+        vy=bullet.speed*(dy/d)
+        bullet.position_x+=vx
+        bullet.position_y+=vy
+      #  checkForHit()  tabdil pos be grid va inke to grid che obj ee hast baad damage va az bein raftan bullet
+
 
 
 
--- Hero.py
+++ Hero.py
@@ -1,18 +1,44 @@
-import Data
+import Data,Bullet
 
 class Hero:
-    def __init__(self,x,y,name):
+    def __init__(self,x,y,name_,side_):
 
-        self.Name=name
+        self.name=name_
+        self.side=side_
         heros_atributes = Heros_Data.Heros_Dic[name]
-        self.Position_x=x
-        self.Position_y=y
-        self.Speed_Rate=heros_atributes["SPEED_RATE"]
-        self.Health=heros_atributes["HEALTH"]
-        self.Bullet=heros_atributes["BULLET"]
-        self.Damage_Rate=heros_atributes["DAMAGE_RATE"]
+        self.position_x=x
+        self.position_y=y
+        self.speed_Rate=heros_atributes["SPEED_RATE"]
+        self.health=heros_atributes["HEALTH"]
+        self.bullet=heros_atributes["BULLET"]
+        self.damage_Rate=heros_atributes["DAMAGE_RATE"]
 
 
+
+
+def herosProcess(currentHeros,currentBullets,Cnt):
+    
+    for hero in currentHeros[0]:
+        if Cnt % hero.speed_Rate == 0:
+            
+            hero.position_x+=1
+            # bayad hero.nextPostion() bashe
+
+
+        if Cnt % hero.damage_Rate == 0:
+            newBullet = Bullet.Bullet(hero.position_x , hero.position_y , hero.position_x+130 ,hero.position_y+130,hero.name,hero.side) # bayad dorost hesabi mosahkhs she in example e
+            currentBullets.append(newBullet)
+
+
+
+
+
+            
+
+    
+
+
+    
 '''
     REQUIERFD:
     
